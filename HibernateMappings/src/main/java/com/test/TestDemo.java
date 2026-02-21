4c3899ab43128d7542d6e060883dd4c4e493d319
Monisha C <sunandammonisha@gmail.com>
2026-02-21 11:50:50 +0530

    OneToMany - ManyToOne

---
+++ HibernateMappings/src/main/java/com/test/TestDemo.java
@@ -0,0 +1,49 @@
+package com.test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
+public class TestDemo {
+
+	public static void main(String[] args) {
+
+		Configuration cfg = new Configuration();
+
+		cfg.configure("hibernate.cfg.xml");
+
+		SessionFactory sf = cfg.buildSessionFactory();
+
+		Session session = sf.openSession();
+
+		Transaction t = session.beginTransaction();
+
+		Order o1 = new Order();
+
+		o1.setO_name("test");
+		o1.setO_price(50);
+
+		Order o2 = new Order();
+
+		o1.setO_name("move");
+		o1.setO_price(200);
+		
+		List<Order> listorder = Arrays.asList(o1,o2);
+		
+		User uobj = new User();
+		
+		uobj.setU_name("Min");
+		uobj.setOrders(listorder);
+		
+		session.persist(uobj);
+		
+		t.commit();
+		
+		session.close();
+	}
+
+}
