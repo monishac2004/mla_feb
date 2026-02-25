a285a6c2358bf17727c553a379ae2eb02def2f02
Monisha C <sunandammonisha@gmail.com>
2026-02-25 09:36:11 +0530

    Add files via upload

---
+++ BeanLifecycleMethods/src/main/java/com/test/ConnectionManager.java
@@ -0,0 +1,43 @@
+package com.test;
+
+import org.springframework.stereotype.Component;
+
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+
+@Component
+public class ConnectionManager {
+	
+	private boolean isConnected = false;
+	
+	@PostConstruct	
+	public void  connect() {
+		System.out.println("Connecting to external server");
+		
+		isConnected = true;
+		
+		System.out.println("Connection established isConnect");
+	}
+	
+	@PreDestroy
+	public void disconnect() {
+		System.out.println("Disconnecting from external server");
+		
+		isConnected = false;
+		
+		System.out.println("Connection closed isConnected : "+isConnected  );
+		
+	}
+	
+	public void operation() {
+		if(isConnected) {
+			System.out.println("Performing operation with active");
+		}
+		else {
+			System.out.println("Operation failed not connected");
+		}
+	}
+	
+	
+
+}
