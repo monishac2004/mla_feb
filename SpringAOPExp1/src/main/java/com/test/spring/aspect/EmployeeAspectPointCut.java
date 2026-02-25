9ea46c916e66f12c5f7cdf8afb2eb07fd88aee5e
Monisha C <sunandammonisha@gmail.com>
2026-02-25 17:23:46 +0530

    Add files via upload

---
+++ SpringAOPExp1/src/main/java/com/test/spring/aspect/EmployeeAspectPointCut.java
@@ -0,0 +1,37 @@
+package com.test.spring.aspect;
+
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+
+@Aspect
+public class EmployeeAspectPointCut {
+	
+	@Before("getNamePointcut()")
+	public void loggingAdvice()
+	{
+		System.out.println("Execution logging advice on getName()");
+	}
+	
+	@Before("getNamePointcut()")
+	public void secondAdvice()
+	{
+		System.out.println("executing secondAdvice on getName()");
+	}
+	
+	@Pointcut("execution(public String getName())")
+	public void getNamePointcut()
+	{
+		
+	}
+	
+	@Before("allMethodsPointCut()")
+	public void allServiceMethodsAdvice() {
+		System.out.println("Before executing service method");
+	}
+	
+	@Pointcut("within(com.test.service.*)")
+	public void allMethodsPointcut() {
+		
+	}
+}
